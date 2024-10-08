15203b5089b12b7f0243d392b94d3f974fbd8d03
Pradeep Kc <pradypkc@gmail.com>
2024-10-09 00:01:56 +0545

    animation changes

--- pages/landing/components/Learn.tsx
+++ pages/landing/components/Learn.tsx
@@ -1,30 +1,30 @@
-"use client";
-import VideoAnimation from "@/components/VideoAnimation";
+'use client';
+import VideoAnimation from '@/components/VideoAnimation';
 // import VideoPlayAnimation from "@/components/VideoPlayAnimation";
-import { useGSAP } from "@gsap/react";
+import { useGSAP } from '@gsap/react';
 // import { useScroll } from "framer-motion";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-import { useRef } from "react";
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+import { useRef } from 'react';
 
 gsap.registerPlugin(useGSAP);
 gsap.registerPlugin(ScrollTrigger);
 
 const Steps = [
   {
-    title: "Explore",
+    title: 'Explore',
     description:
       "Browse a collection of iconic masterpieces carefully handpicked by our expert curators from Sotheby's and Christie's. ",
   },
   {
-    title: "Invest",
+    title: 'Invest',
     description:
-      "Start investing in fractional shares of legacy masterpieces with no auction house markups, no gallery markups, no hidden true-up fees.",
+      'Start investing in fractional shares of legacy masterpieces with no auction house markups, no gallery markups, no hidden true-up fees.',
   },
   {
-    title: "Earn",
+    title: 'Earn',
     description:
-      "Watch your investment grow in value through auction exits, rents from exhibitions in museums and galleries, loyalty from NFT recreations and consumer merchandise and many more..",
+      'Watch your investment grow in value through auction exits, rents from exhibitions in museums and galleries, loyalty from NFT recreations and consumer merchandise and many more..',
   },
 ];
 
@@ -41,38 +41,30 @@ const Learn = () => {
   useGSAP(() => {
     ScrollTrigger.create({
       trigger: containerRef.current,
-      start: "top top",
-      end: "bottom bottom",
+      start: 'top top',
+      end: 'bottom bottom',
       pin: leftRef.current,
     });
   });
   return (
-    <section
-      ref={containerRef}
-      className="w-[90vw] mx-auto sm:w-[85vw] min-h-screen "
-    >
-      <div className="flex flex-col md:flex-row">
-        <div className="h-screen md:w-1/2 " ref={leftRef}>
+    <section ref={containerRef} className='w-[90vw] mx-auto sm:w-[85vw] min-h-screen '>
+      <div className='flex flex-col md:flex-row'>
+        <div className='h-screen md:w-1/2 ' ref={leftRef}>
           {/* <VideoPlayAnimation currentIndex={currentIndex} /> */}
           <VideoAnimation />
         </div>
-        <div className="md:w-1/2 flex flex-col gap-8 justify-center z-50 ">
+        <div className='md:w-1/2 flex flex-col gap-8 justify-center z-50 '>
           {Steps.map((item, index) => {
             return (
-              <div
-                key={index}
-                className=" max-w-[800px] h-[80vh] flex items-center justify-center"
-              >
+              <div key={index} className='max-w-[800px] h-[80vh] flex items-center justify-center'>
                 {item.title && (
-                  <div className="flex gap-4 items-start justify-center">
-                    <span className="text-black/30 text-[40px] italic leading-[52.4px] md:mt-4">{`0${
-                      index + 1
-                    }`}</span>
-                    <div className="flex flex-col gap-2">
-                      <h4 className="text-[50px] sm:text-[75px] md:text-[100px] text-balance leading-[65px] sm:leading-[100px] md:leading-[131px] italic font-medium">
+                  <div className='flex gap-4 items-start justify-center'>
+                    <span className='text-black/30 text-[40px] italic leading-[52.4px] md:mt-4'>{`0${index + 1}`}</span>
+                    <div className='flex flex-col gap-2'>
+                      <h4 className='text-[50px] sm:text-[75px] md:text-[100px] text-balance leading-[65px] sm:leading-[100px] md:leading-[131px] italic font-medium'>
                         {item.title}
                       </h4>
-                      <p className=" text-[16px] leading-[20.4px] sm:text-[20px] sm:leading-[26.2px]">
+                      <p className=' text-[16px] leading-[20.4px] sm:text-[20px] sm:leading-[26.2px]'>
                         {item.description}
                       </p>
                     </div>
