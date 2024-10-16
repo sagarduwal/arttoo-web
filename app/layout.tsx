dbea115b4a0f81b18931bdbc38386187b271448a
Pradeep Kc <pradypkc@gmail.com>
2024-10-16 15:47:46 +0545

    favion added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import Head from 'next/head';
 
 export const metadata: Metadata = {
   title: 'Arttoo',
@@ -13,6 +14,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <Head>
+        <link rel='icon' href='/favicon.ico' sizes='16x16' />
+        <link rel='icon' href='/favicon-32x32.png' sizes='32x32' />
+        <link rel='icon' href='/favicon-96x96.png' sizes='96x96' />
+        <link rel='apple-touch-icon' href='/apple-icon-180x180.png' />
+        <link rel='manifest' href='/site.webmanifest' />
+        <meta name='theme-color' content='#FFFFFF' />
+      </Head>
       <body className={`antialiased`}>{children}</body>
     </html>
   );
