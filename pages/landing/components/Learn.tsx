163d5fd3d36611da1bcc9512411c1aec8cf4a7fe
Pradeep Kc <pradypkc@gmail.com>
2024-10-18 20:34:14 +0545

    ios fixes

--- pages/landing/components/Learn.tsx
+++ pages/landing/components/Learn.tsx
@@ -74,7 +74,7 @@ const Learn: React.FC = () => {
             <video
               ref={videoRef}
               className='w-full h-full object-cover relative z-10'
-              autoPlay={isIOS}
+              autoPlay
               playsInline
               muted
               preload='auto'
