4a988ab92c843d64081dfb68fc78723818c2e0eb
Pradeep Kc <pradypkc@gmail.com>
2024-10-18 23:26:10 +0545

    ios fixes test

--- pages/landing/components/Learn.tsx
+++ pages/landing/components/Learn.tsx
@@ -24,8 +24,9 @@ const Steps = [
   },
 ];
 
-const videoSrc = 'section3-highres.mp4';
-// const videoSrc = 'https://www.apple.com/media/us/mac-pro/2013/16C1b6b5-1d91-4fef-891e-ff2fc1c1bb58/videos/macpro_main_desktop.mp4';
+// const videoSrc = 'section3-highres.mp4';
+const videoSrc =
+  'https://www.apple.com/media/us/mac-pro/2013/16C1b6b5-1d91-4fef-891e-ff2fc1c1bb58/videos/macpro_main_desktop.mp4';
 // const videoSrc = 'steps-43.mp4';
 
 const Learn: React.FC = () => {
