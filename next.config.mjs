2c213a5d4bc9c4bfe3bdca42ed3573b01bd5a10c
suman <sumanrajkhanal@gmail.com>
2024-10-21 23:58:41 +0545

    change in the animation scroll

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/**
+ * @format
+ * @type {import('next').NextConfig}
+ */
+
+const nextConfig = {
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+};
 
 export default nextConfig;
