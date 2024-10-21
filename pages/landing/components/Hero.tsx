645c80f43ed8cabcd81807aefaa2da4c1633fcf0
Pradeep Kc <pradypkc@gmail.com>
2024-10-22 02:50:26 +0545

    arrow circle

--- pages/landing/components/Hero.tsx
+++ pages/landing/components/Hero.tsx
@@ -45,11 +45,22 @@ const Hero = () => {
 
         <motion.div className='absolute inset-0 w-full bg-black' style={{ opacity }} />
         <button
-          className='border-4 border-white rounded-2xl w-[32px] h-[48px] absolute animate-bounce bottom-10 left-1/2 -ml-2 z-10 group'
+          className='border-2 border-white rounded-full w-[46px] h-[46px] absolute animate-bounce bottom-10 left-1/2 -ml-2 z-10 group'
           onClick={() => ScrollTo('artworks')}
         >
-          <span className='absolute top-2 left-1/2 h-[10px] w-1 bg-white rounded-sm -ml-[2px] transition-all duration-300 ease-in-out group-hover:m-0 group-hover:-top-1 group-hover:-left-1 group-hover:w-[32px] group-hover:h-[48px] group-hover:rounded-2xl' />
-          <span className='absolute top-2 left-1/2 transform -translate-x-1/2 h-[10px] w-1 bg-white rounded-sm transition-all duration-300 ease-in-out group-hover:bg-black' />
+          <svg
+            className='absolute top-2 left-1/2 -ml-3 text-white h-6 w-6 transition-all duration-300 ease-in-out group-hover:text-black'
+            xmlns='http://www.w3.org/2000/svg'
+            width='200'
+            height='200'
+            fill='currentColor'
+            stroke='currentColor'
+            strokeWidth='0'
+            viewBox='0 0 24 24'
+          >
+            <path d='M4.97 13.22a.75.75 0 011.06 0L11 18.19V3.75a.75.75 0 011.5 0v14.44l4.97-4.97a.749.749 0 011.275.326.749.749 0 01-.215.734l-6.25 6.25a.75.75 0 01-1.06 0l-6.25-6.25a.75.75 0 010-1.06z'></path>
+          </svg>
+          <span className='absolute w-0 h-0 top-1/2 l-1/2 -translate-x-1/2 -translate-y-1/2 bg-white rounded-full transition-all duration-300 ease-in-out group-hover:w-[46px] group-hover:h-[46px] z-[-1]' />
         </button>
       </div>
     </section>
